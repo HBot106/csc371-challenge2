e006d1bccf6ebb9a91105672f35bfc15ccf37b6d
Henry Luengas <gyatso@pop-os.localdomain>
2020-01-20 17:44:34 -0800

    challenge done

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 1.0f;
+
+    private int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        // flip a coin and try to spawn a ball
+        if (Random.Range(0.0f, 1.0f) > 0.5f)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefabs[index%3], spawnPos, ballPrefabs[index%3].transform.rotation);
+            index += 1;
+        }
+    }
+
+}
